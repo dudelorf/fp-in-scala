e748b69949143e45c68108df16a2fc1984ddfb90
Eric Kinzel <ekinzel@rewardsnetwork.com>
2020-03-16 21:10:42 -0500

    Working on ch3

--- src/main/scala/fp/Ch3.scala
+++ src/main/scala/fp/Ch3.scala
@@ -19,10 +19,10 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
-    
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
     as match {
-      case Nil => z
+      case Nil         => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 }
@@ -56,11 +56,36 @@ object Ch3 {
   def init[A](l: List[A]): List[A] =
     l match {
       case Nil | Cons(_, Nil) => Nil
-      case Cons(head, tail) => Cons(head, init(tail))
+      case Cons(head, tail)   => Cons(head, init(tail))
     }
-    
+
   def length[A](as: List[A]): Int =
     List.foldRight(as, 0)((_, acc) => acc + 1)
+
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil              => z
+    case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+  }
+
+  def sum(l: List[Int]) = foldLeft(l, 0)(_ + _)
+
+  def product(l: List[Int]) = foldLeft(l, 0)(_ * _)
+
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((lt, _) => lt + 1)
+
+  def reverse[A](l: List[A]) =
+    foldLeft(l, Nil: List[A])((acc, h) => Cons(h, acc))
+
+  def foldLeft2[A, B](as: List[A], z: B)(f: (B, A) => B): B = 
+    List.foldRight(reverse(as), z)((a, b) => f(b, a))
     
-  
+  def foldRight2[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    foldLeft(reverse(as), z)(f)
+    
+  def append[A](as: List[A], a: A): List[A] =
+    foldLeft(List(a), as)((acc, h) => Cons(h, acc))
+    
+  def concatenate[A](as1: List[A], as2: List[A]): List[A] =
+    foldLeft(as1, as2)(append)
 }
