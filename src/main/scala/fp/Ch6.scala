009233b8aad643e437cfa5c4a26f41bba47ae399
Eric Kinzel <ericrkinzel@gmail.com>
2020-05-29 07:44:56 -0500

    Working on ch6

--- src/main/scala/fp/Ch6.scala
+++ src/main/scala/fp/Ch6.scala
@@ -20,6 +20,8 @@ object Ch6 {
     (i / (Int.MaxValue.toDouble + 1), r)
   }
 
+  def int: Rand[Int] = _.nextInt
+
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
     val (i, r1) = nonNegativeInt(rng)
     val (d, r2) = double(r1)
@@ -55,4 +57,34 @@ object Ch6 {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
+
+  def double2 =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))
+
+  def ints2(counts: Int)(rng: RNG): Rand[List[Int]] =
+    sequence(List.fill(counts)(int))
+
+  def flatMap[A,B](s: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r) = s(rng)
+      g(a)(r)
+    }
+
+  def mapFm[A, B](s: Rand[A])(f: A => B): Rand[B] = ???
+
+  def map2Fm[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+
 }
